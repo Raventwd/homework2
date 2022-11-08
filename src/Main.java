28136e59cfca67b873fa6c85ee6088fd4daed9cb
raventwd <maximowlmaster@mail.ru>
2022-11-08 12:53:34 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        //Homework 1
+        //Task 1
         var dog = 8.0;
         System.out.println(dog);
         dog += 4;
         System.out.println(dog);
         dog -= 3.5;
         System.out.println(dog);
-
+        //Task 2
         var cat = 3.6;
         System.out.println(cat);
         cat += 4;
         System.out.println(cat);
         cat -= 1.6;
         System.out.println(cat);
-
+        //Task 3
         var paper = 763789;
         System.out.println(paper);
         paper += 4;
@@ -21,13 +23,15 @@ public class Main {
         paper -= 7639;
         System.out.println(paper);
 
+        //Homework 2
+        //Task 1
         var friend = 19;
         System.out.println(friend);
         friend += 2;
         System.out.println(friend);
         friend /= 7;
         System.out.println(friend);
-
+        //Task 2
         var frog = 3.5;
         System.out.println(frog);
         frog *=10;
@@ -37,5 +41,17 @@ public class Main {
         frog +=4;
         System.out.println(frog);
 
+        //Homework 3
+        //Task 1
+        var FirstFighterWeight = 78.2;
+        var SecondFighterWeight = 82.7;
+        var WeightSummary = FirstFighterWeight + SecondFighterWeight;
+        var FirstWeightDifference = (SecondFighterWeight - FirstFighterWeight);
+        System.out.println(WeightSummary);
+        System.out.println(FirstWeightDifference);
+        //Task 2
+        var SecondWeightDifference = (SecondFighterWeight % FirstFighterWeight);
+        System.out.println(SecondWeightDifference);
+        //Task 3
     }
 }
